041d00695f68a28accf462429e64df9bae85439c
노준호 <njh910505@gmail.com>
2020-05-09 21:33:06 +0900

    client, server 분리

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql_practice/resolvers";
 
 const server = new GraphQLServer({
-  typeDefs: "graphql_practice/schema.graphql",
+  typeDefs: "server/graphql_practice/schema.graphql",
   resolvers
 });
 
